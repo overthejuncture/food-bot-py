dc5bf35e2f12f32538f400d275b1645d12738c7d
overthejuncture <overthejuncture@gmail.com>
2022-06-12 22:48:40 +0300

    some some

--- project/bot/management/commands/botcheck.py
+++ project/bot/management/commands/botcheck.py
@@ -6,6 +6,11 @@ from telegram.ext import CommandHandler
 from bot.models import Choise
 from django.core.management.base import BaseCommand
 import urllib.parse
+import random
+from telegram import InlineKeyboardButton
+from typing import Union, List
+from telegram import InlineKeyboardButton
+from telegram import InlineKeyboardMarkup
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
@@ -19,8 +24,8 @@ class Command(BaseCommand):
             reply_text = ''
             strings = []
             for idx, choise in enumerate(choises):
-                txt = "{idx}. {text}"
-                strings.append(reply_text + txt.format(idx=idx+1, text=choise.text))
+                txt = "{idx}. {text} {active}"
+                strings.append(reply_text + txt.format(idx=idx+1, text=choise.text, active = 'yes' if choise.active else 'no'))
             if strings:
                 update.message.reply_text("\n".join(strings))
             else:
@@ -28,13 +33,41 @@ class Command(BaseCommand):
 
         def add(update: Update, context: CallbackContext):
             if not context.args:
+                context.bot.send_message(chat_id=update.effective_chat.id, text="Нужно ввести текст после команды, например: /add Фуагра со спаржей")
                 return
+            ch = Choise(text=' '.join(context.args))
+            ch.save()
             reply_text = ' '.join(context.args)
             context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
 
+        def choose(update: Update, context: CallbackContext):
+            choises = Choise.objects.all()
+            choise = random.choice(choises)
+            buttons = [
+                InlineKeyboardButton("Да", callback_data="yes")
+            ]
+            reply_markup = InlineKeyboardMarkup(build_menu(buttons, n_cols=1))
+            context.bot.send_message(chat_id=update.effective_chat.id, text=choise.text, reply_markup=reply_markup)
+            pass
+
+        def build_menu(
+            buttons: List[InlineKeyboardButton],
+            n_cols: int,
+            header_buttons: Union[InlineKeyboardButton, List[InlineKeyboardButton]]=None,
+            footer_buttons: Union[InlineKeyboardButton, List[InlineKeyboardButton]]=None
+        ) -> List[List[InlineKeyboardButton]]:
+            menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+            if header_buttons:
+                menu.insert(0, header_buttons if isinstance(header_buttons, list) else [header_buttons])
+            if footer_buttons:
+                menu.append(footer_buttons if isinstance(footer_buttons, list) else [footer_buttons])
+            return menu
+
         start_handler = CommandHandler('start', start)
+        choose_handler = CommandHandler('choose', choose)
         add_handler = CommandHandler('add', add)
         dispatcher.add_handler(start_handler)
+        dispatcher.add_handler(choose_handler)
         dispatcher.add_handler(add_handler)
 
         updater.start_polling()
--- project/bot/models.py
+++ project/bot/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Choise(models.Model):
     text = models.CharField(max_length=200)
+    active = models.BooleanField(default = True)
 
     def __str__(self):
         return self.text
