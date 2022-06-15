768c431d05a7a92fcd11d3550ca7503d629a70bc
overthejuncture <overthejuncture@gmail.com>
2022-06-15 19:27:08 +0300

    new models

--- project/bot/models.py
+++ project/bot/models.py
@@ -6,3 +6,16 @@ class Choise(models.Model):
 
     def __str__(self):
         return self.text
+
+class User(models.Model):
+    telegram_id = models.TextField()
+
+    def __str__(self):
+        return "{id} {telegram_id}".format(self.id, self.telegram_id)
+
+class ChoiseUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    choise = models.ForeignKey(Choise, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return "{user_id} {choise_id}".format(self.user_id, self.choise_id)
---
+++ project/bot/utils.py
@@ -0,0 +1,38 @@
+from typing import Union, List
+from telegram import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
+import json
+
+def build_menu(
+    buttons: List[InlineKeyboardButton],
+    n_cols: int,
+    header_buttons: Union[InlineKeyboardButton, List[InlineKeyboardButton]]=None,
+    footer_buttons: Union[InlineKeyboardButton, List[InlineKeyboardButton]]=None
+) -> List[List[InlineKeyboardButton]]:
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, header_buttons if isinstance(header_buttons, list) else [header_buttons])
+    if footer_buttons:
+        menu.append(footer_buttons if isinstance(footer_buttons, list) else [footer_buttons])
+    return menu
+
+def getKeyboardForChoises(start, limit, choises, show_previous_button):
+    buttons = []
+    strings = []
+    # put this in separate function with params for start and limit
+    for idx, choise in enumerate(choises):
+        strings.append("{idx} {text}".format(idx=idx+1, text=choise.text))
+        buttons.append(
+            InlineKeyboardButton(idx+1, callback_data=json.dumps({"id": choise.id}))
+        )
+    more_buttons = []
+    if start > 0:
+        more_buttons.append(InlineKeyboardButton("Previous", callback_data=json.dumps({"start": start - limit, "action":"prev"})))
+    if show_previous_button:
+        more_buttons.append(InlineKeyboardButton("Next", callback_data=json.dumps({"start": start + limit, "action":"next"})))
+
+    reply_markup = InlineKeyboardMarkup(build_menu(buttons, n_cols=2,footer_buttons=more_buttons))
+    return reply_markup, strings
+
